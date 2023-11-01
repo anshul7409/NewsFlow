48003558ff3a9084e4bf0767ff93254a6c2227a7
Anshul Rawat <79444489+anshul7409@users.noreply.github.com>
2023-11-01 20:29:16 +0530

    data fetching

---
+++ Database creation/data_fetchin.py
@@ -0,0 +1,39 @@
+import requests
+from bs4 import BeautifulSoup as bs
+from urllib.request import urlopen as uReq
+
+news_cat_url =   "https://timesofindia.indiatimes.com/topic/" + "dehradun"
+
+urlclient = uReq(news_cat_url)
+
+news_page = urlclient.read()
+
+news_html = bs(news_page,'html.parser')
+
+boxes = news_html.findAll("div",{"class":"uwU81"})
+
+lis = []
+if (len(boxes))>0:
+ for i in range((len(boxes))):
+   lis.append(boxes[i].a['href'])
+else:
+   print("no news available")
+   pass
+
+print(lis)
+
+news_html = []
+for i in range(0,len(lis)):
+   news_req = requests.get(lis[i])
+   news_content = bs(news_req.text,'html.parser')
+   box = news_content.find("div",{"class":"JuyWl"})
+   news_html.append(box)
+
+news_data = []
+for element in news_html:
+    if element is not None and hasattr(element, 'text'):
+        news_data.append(element.text)
+    else:
+        news_data.append("N/A")
+
+print(news_data)
