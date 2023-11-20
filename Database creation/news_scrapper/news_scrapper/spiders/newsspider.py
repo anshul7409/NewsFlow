b0c0ae4c6a34e5ba6624490b789bcbb022768370
Anshul Rawat <79444489+anshul7409@users.noreply.github.com>
2023-11-20 10:47:28 +0530

    done!

--- Database creation/news_scrapper/news_scrapper/spiders/newsspider.py
+++ Database creation/news_scrapper/news_scrapper/spiders/newsspider.py
@@ -1,15 +1,12 @@
 import scrapy
-
-
-import scrapy
+import uuid
 
 class NewsspiderSpider(scrapy.Spider):
     name = "newsspider"
     allowed_domains = ["timesofindia.indiatimes.com"]
-    start_urls = ['https://timesofindia.indiatimes.com/topic/'+'tech']
+    start_urls = ['https://timesofindia.indiatimes.com/topic/'+'isreal']
 
     def parse(self, response):
-        # Your scraping logic to extract data from the initial page
         news_data = response.css('div.uwU81')
         if news_data:
             for news_sample in news_data:
@@ -26,26 +23,23 @@ class NewsspiderSpider(scrapy.Spider):
                       date_time = date_time_text[0]
                       srcc = ''
                 
-                    
-                # Extracted data from the initial page
                 item = {
+                    'unique_id': str(uuid.uuid4())[:8],  # Generate a unique ID for each item and take the first 8 characters
                     'url': response.urljoin(news_sample.css('a').attrib['href']),
                     'headline': meta_.css('div.fHv_i span::text').get(),
                     'Src': srcc,
                     'date_time': date_time,
                 }
 
-                # Follow the URL and parse the news page
                 yield scrapy.Request(item['url'], callback=self.parse_news_page, meta={'item': item})
         else:
-            yield {'url':'','headline': '','Src':'','date_time': '','description':''}
+            yield {'unique_id': str(uuid.uuid4())[:8], 'url':'','headline': '','Src':'','date_time': '','description':''}
 
     def parse_news_page(self, response):
         item = response.meta['item']
-        # Add more fields to the item based on the data from the news page
         news_content = response.css('div.JuyWl ::text')
         if news_content:
            item['description'] = news_content.getall()
-       # item['additional_field'] = response.css('div.additional_data::text').get()
-
+        else:
+           item['description'] = ["N/A"] 
         yield item
