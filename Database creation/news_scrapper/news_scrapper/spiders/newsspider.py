fc8e8684171df7a13b57b1618aa5503f792ecebf
Anshul Rawat <79444489+anshul7409@users.noreply.github.com>
2023-11-21 09:10:51 +0530

    !

--- Database creation/news_scrapper/news_scrapper/spiders/newsspider.py
+++ Database creation/news_scrapper/news_scrapper/spiders/newsspider.py
@@ -23,21 +23,14 @@ class NewsspiderSpider(scrapy.Spider):
                       date_time = date_time_text[0]
                       srcc = ''
                 
-<<<<<<< HEAD
                 item = {
                     'unique_id': str(uuid.uuid4())[:8],  # Generate a unique ID for each item and take the first 8 characters
-=======
-                    
-                # Extracted data from the initial page
-                item = {
->>>>>>> master
                     'url': response.urljoin(news_sample.css('a').attrib['href']),
                     'headline': meta_.css('div.fHv_i span::text').get(),
                     'Src': srcc,
                     'date_time': date_time,
                 }
 
-<<<<<<< HEAD
                 yield scrapy.Request(item['url'], callback=self.parse_news_page, meta={'item': item})
 
     def parse_news_page(self, response):
@@ -48,19 +41,3 @@ class NewsspiderSpider(scrapy.Spider):
         else:
            item['description'] = ["premium news"] 
         yield item
-=======
-                # Follow the URL and parse the news page
-                yield scrapy.Request(item['url'], callback=self.parse_news_page, meta={'item': item})
-        else:
-            yield {'url':'','headline': '','Src':'','date_time': '','description':''}
-
-    def parse_news_page(self, response):
-        item = response.meta['item']
-        # Add more fields to the item based on the data from the news page
-        news_content = response.css('div.JuyWl ::text')
-        if news_content:
-           item['description'] = news_content.getall()
-       # item['additional_field'] = response.css('div.additional_data::text').get()
-
-        yield item
->>>>>>> master
