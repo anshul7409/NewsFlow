5b22f6285060dbf743d958065b6e1fed5e08fe6f
Anshul Rawat <79444489+anshul7409@users.noreply.github.com>
2023-11-22 08:12:20 +0530

    !

--- Database creation/news_scrapper/news_scrapper/items.py
+++ Database creation/news_scrapper/news_scrapper/items.py
@@ -10,3 +10,13 @@ class NewsScrapperItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
     pass
+
+class NewsItem(scrapy.Item):
+    unique_id = scrapy.Field()
+    url = scrapy.Field()
+    headline = scrapy.Field()
+    Src = scrapy.Field()
+    date_time = scrapy.Field()
+    description = scrapy.Field()
+    len = scrapy.Field()
+    len_summary = scrapy.Field()
