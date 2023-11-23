67fef5611aaba54d9202210eade731fbb95a06b6
Anshul Rawat <79444489+anshul7409@users.noreply.github.com>
2023-11-23 09:20:24 +0530

    !!

--- Database creation/news_scrapper/news_scrapper/spiders/newsspider.py
+++ Database creation/news_scrapper/news_scrapper/spiders/newsspider.py
@@ -7,7 +7,8 @@ from news_scrapper.items import NewsItem
 class NewsspiderSpider(scrapy.Spider):
     name = "newsspider"
     allowed_domains = ["timesofindia.indiatimes.com"]
-    start_urls = ['https://timesofindia.indiatimes.com/topic/'+'picture']
+    topic = 'picture'
+    start_urls = ['https://timesofindia.indiatimes.com/topic/'+ topic]
     # openai_summarizer = openai_summarize.OpenAISummarize(Config.OPENAI_KEY)
 
     def parse(self, response):
